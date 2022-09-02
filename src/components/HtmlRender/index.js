5da7b5bb555bc1d8a10138dbae2cac6101d28834
kongkx <kongkx.yang@gmail.com>
2022-09-03 00:28:34 +0800

    define HtmlRender tagStyles

--- src/components/HtmlRender/index.js
+++ src/components/HtmlRender/index.js
@@ -17,31 +17,85 @@ const renderers = {
 }
 
 const defaultTextProps = { selectable: true }
-const baseStyle = {
-  fontSize: 16
-  // lineHeight: 21
-}
 
-function RenderHtml({ tagsStyles, ...props }) {
+function RenderHtml({ tagsStyles, baseStyle, ...props }) {
   const tw = useTailwind()
   const navigation = useNavigation()
 
-  const styles = useMemo(
-    () => ({
-      body: tw('text-gray-700'),
-      // h2: tw('text-xl'),
-      // h3: tw('text-lg'),
-      // h2: tw('border-b pb-[6px] border-gray-300'),
-      pre: { backgroundColor: '#f6f6f6', padding: 12 },
+  const styles = useMemo(() => {
+    const baseFontSize = baseStyle?.fontSize || 16
+    return {
+      body: {
+        ...tw('text-gray-700'),
+        fontSize: baseFontSize,
+        lineHeight: baseFontSize * 1.33
+      },
+      h1: {
+        fontSize: (28 / 16) * baseFontSize,
+        lineHeight: (28 / 16) * baseFontSize * 1.1,
+        marginTop: 1.25 * baseFontSize,
+        marginBottom: baseFontSize
+      },
+      h2: {
+        fontSize: (24 / 16) * baseFontSize,
+        lineHeight: (24 / 16) * baseFontSize * 1.1,
+        marginTop: 1.25 * baseFontSize,
+        marginBottom: baseFontSize
+      },
+      h3: {
+        fontSize: (20 / 16) * baseFontSize,
+        lineHeight: (20 / 16) * baseFontSize * 1.1,
+        marginTop: 1.25 * baseFontSize,
+        marginBottom: baseFontSize
+      },
+      h4: {
+        fontSize: (18 / 16) * baseFontSize,
+        lineHeight: (18 / 16) * baseFontSize * 1.1,
+        marginTop: baseFontSize / 1.5,
+        marginBottom: baseFontSize / 2
+      },
+      h5: {
+        fontSize: (16 / 16) * baseFontSize,
+        lineHeight: (16 / 16) * baseFontSize * 1.1,
+        marginVertical: baseFontSize / 2
+      },
+      h6: {
+        fontSize: (14 / 16) * baseFontSize,
+        lineHeight: (14 / 16) * baseFontSize * 1.1,
+        marginVertical: baseFontSize / 2
+      },
+      hr: {
+        marginVertical: baseFontSize
+      },
+      pre: {
+        backgroundColor: '#f6f6f6',
+        paddingHorizontal: (12 / 16) * baseFontSize,
+        lineHeight: 1.25 * baseFontSize
+      },
       code: tw('text-sm'),
       ul: {
-        paddingLeft: 12
+        paddingLeft: 12,
+        marginTop: 0
+      },
+      li: {
+        lineHeight: 1.5 * baseFontSize
+      },
+      p: {
+        marginTop: 0,
+        lineHeight: 1.25 * baseFontSize,
+        marginBottom: baseFontSize
       },
       a: tw('no-underline'),
+      blockquote: {
+        marginLeft: 0,
+        paddingLeft: baseFontSize * 1.5,
+        paddingVertical: baseFontSize * 0.25,
+        borderLeftWidth: 2,
+        borderLeftColor: '#111111'
+      },
       ...(tagsStyles || {})
-    }),
-    [tagsStyles]
-  )
+    }
+  }, [tagsStyles, baseStyle])
   const renderersProps = useMemo(() => {
     return {
       a: {
@@ -69,13 +123,20 @@ function RenderHtml({ tagsStyles, ...props }) {
       renderers={renderers}
       renderersProps={renderersProps}
       defaultTextProps={defaultTextProps}
-      baseStyle={baseStyle}
       {...props}
     />
   )
 }
 
 RenderHtml.propTypes = {
-  tagsStyles: PropTypes.object
+  tagsStyles: PropTypes.object,
+  baseStyle: PropTypes.object
 }
+
+RenderHtml.defaultProps = {
+  baseStyle: {
+    fontSize: 16
+  }
+}
+
 export default memo(RenderHtml)
---
+++ src/mock/html.js
@@ -0,0 +1,73 @@
+export const html = `
+
+<h1>Headings</h1>
+
+<h1>h1. Bootstrap heading</h1>
+<h2>h2. Bootstrap heading</h2>
+
+<p>Ullamco pariatur pariatur cupidatat amet nulla ea tempor dolor ea ullamco nulla. Ad nulla culpa ipsum proident. Commodo et pariatur minim exercitation sit ut nisi elit sunt ut laborum ut commodo. Mollit qui sunt ipsum et velit eiusmod nostrud tempor quis do ullamco ex laboris mollit. Cillum consectetur ea sint consectetur.</p>
+<p>Ullamco pariatur pariatur cupidatat amet nulla ea tempor dolor ea ullamco nulla. Ad nulla culpa ipsum proident. Commodo et pariatur minim exercitation sit ut nisi elit sunt ut laborum ut commodo. Mollit qui sunt ipsum et velit eiusmod nostrud tempor quis do ullamco ex laboris mollit. Cillum consectetur ea sint consectetur.</p>
+
+<h3>h3. Bootstrap heading</h3>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident eos officiis culpa, accusamus debitis recusandae vel, sed consequatur porro possimus a magnam voluptatum ut, animi alias excepturi ipsam dolorum dolorem?</p>
+<pre>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias dolores tenetur harum unde, consequatur, ullam odit ab similique error cumque eligendi magni? Enim corporis nesciunt quis
+ea quibusdam quidem eos.
+</pre>
+<h4>h4. Bootstrap heading</h4>
+<h5>h5. Bootstrap heading</h5>
+<h6>h6. Bootstrap heading</h6>
+
+
+
+<hr />
+
+<p>loremLabore cillum exercitation enim aute id. Ex ullamco aliqua veniam exercitation laboris incididunt tempor mollit aliquip minim laboris enim magna laboris. Labore ex laboris nulla commodo mollit nisi laboris consectetur et. Esse aliqua reprehenderit tempor irure dolor ipsum aliqua Lorem aute fugiat eu anim eu.</p>
+
+<hr />
+
+<p>You can use the mark tag to <mark>highlight</mark> text.</p>
+<p><del>This line of text is meant to be treated as deleted text.</del></p>
+<p><s>This line of text is meant to be treated as no longer accurate.</s></p>
+<p><ins>This line of text is meant to be treated as an addition to the document.</ins></p>
+<p><u>This line of text will render as underlined</u></p>
+<p><small>This line of text is meant to be treated as fine print.</small></p>
+<p><strong>This line rendered as bold text.</strong></p>
+<p><em>This line rendered as italicized text.</em></p>
+
+<blockquote class="blockquote text-center">
+  <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+  <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+</blockquote>
+
+<h2>List</h2>
+
+<ul class="list-unstyled">
+  <li>Lorem ipsum dolor sit amet</li>
+  <li>Consectetur adipiscing elit</li>
+  <li>Integer molestie lorem at massa</li>
+  <li>Facilisis in pretium nisl aliquet</li>
+  <li>Nulla volutpat aliquam velit
+    <ul>
+      <li>Phasellus iaculis neque</li>
+      <li>Purus sodales ultricies</li>
+      <li>Vestibulum laoreet porttitor sem</li>
+      <li>Ac tristique libero volutpat at</li>
+    </ul>
+  </li>
+  <li>Faucibus porta lacus fringilla vel</li>
+  <li>Aenean sit amet erat nunc</li>
+  <li>Eget porttitor lorem</li>
+</ul>
+
+<h3>
+Ordered List
+</h3>
+
+<ol>
+  <li>lorem</li>
+  <li>lorem</li>
+  <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi similique nobis beatae sed possimus perferendis iste adipisci id? Ab cumque corporis eveniet, facere optio fuga vel rem inventore soluta tempora?</li>
+</ol>
+
+`
