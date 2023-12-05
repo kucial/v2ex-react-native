5cc0bf9bf9af9585d529d583c4c58a67b37645e7
kongkx <kongkx.yang@gmail.com>
2023-12-05 22:10:47 +0800

    update NodesScreen style

--- src/screens/NodesScreen/CollectedNodes.tsx
+++ src/screens/NodesScreen/CollectedNodes.tsx
@@ -17,38 +17,42 @@ export default function CollectedNodes(props: { data: NodeExtra[] }) {
 
   return (
     <MaxWidthWrapper>
-      <View className="flex flex-row flex-wrap px-1 py-2">
-        {data.map((node) => (
-          <View key={node.name} className="basis-1/2 px-1 py-1">
-            <Pressable
-              className={classNames(
-                'py-2 px-2 rounded-lg',
-                'flex flex-row items-center',
-                'active:opacity-60',
-              )}
-              style={[styles.layer2]}
-              onPress={() => {
-                navigation.navigate('node', {
-                  name: node.name,
-                })
-              }}>
-              <Image
-                className="w-[44px] h-[44px]"
-                source={{ uri: node.avatar_large }}></Image>
-              <View className="ml-3 pt-1">
-                <Text style={styles.text}>{node.title}</Text>
-                <View className="mt-1 flex flex-row items-center">
-                  <View className="mr-1">
-                    <DocumentIcon size={12} color={theme.colors.text_meta} />
+      <View className="px-1">
+        <View
+          style={styles.layer1}
+          className="flex flex-row flex-wrap px-1 py-2">
+          {data.map((node) => (
+            <View key={node.name} className="basis-1/2 px-1 py-1">
+              <Pressable
+                className={classNames(
+                  'py-2 px-2 rounded-lg',
+                  'flex flex-row items-center',
+                  'active:opacity-60',
+                )}
+                style={[styles.layer2]}
+                onPress={() => {
+                  navigation.navigate('node', {
+                    name: node.name,
+                  })
+                }}>
+                <Image
+                  className="w-[44px] h-[44px]"
+                  source={{ uri: node.avatar_large }}></Image>
+                <View className="ml-3 pt-1">
+                  <Text style={styles.text}>{node.title}</Text>
+                  <View className="mt-1 flex flex-row items-center">
+                    <View className="mr-1">
+                      <DocumentIcon size={12} color={theme.colors.text_meta} />
+                    </View>
+                    <Text className="text-xs" style={styles.text_meta}>
+                      {node.topics}
+                    </Text>
                   </View>
-                  <Text className="text-xs" style={styles.text_meta}>
-                    {node.topics}
-                  </Text>
                 </View>
-              </View>
-            </Pressable>
-          </View>
-        ))}
+              </Pressable>
+            </View>
+          ))}
+        </View>
       </View>
     </MaxWidthWrapper>
   )
